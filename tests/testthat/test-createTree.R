8a6c59e45d27937a24e33c94794e59bc9dcecb76
lgnbhl <felix.luginbuhl@protonmail.ch>
2025-10-31 09:50:27 +0100

    fix createTree() test

--- tests/testthat/test-createTree.R
+++ tests/testthat/test-createTree.R
@@ -1,24 +1,22 @@
-test_that("createTree works as expected", {
+test_that("createTree creates a list", {
   df <- data.frame(
     pkg = c("muiTreeView", "muiTreeView"),
     functions = c("SimpleTreeView", "RichTreeView")
   )
 
   treeItems <- muiTreeView::createTree(df)
-
-  treeItems2 <- list(
-    list(
-      label = 'muiTreeView',
-      id = 'muiTreeView',
-      children = list(
-        list(
-          label = 'SimpleTreeView', id = 'SimpleTreeView'
-        ),
-        list(
-          label = 'RichTreeView', id = 'RichTreeView'
-        )
-      )
-    )
-  )
-  expect_equal(treeItems, treeItems2)
+  
+  # https://stackoverflow.com/a/13433689
+  depth <- function(this,thisdepth=0){
+    if(!is.list(this)){
+      return(thisdepth)
+    }else{
+      return(max(unlist(lapply(this,depth,thisdepth=thisdepth+1))))    
+    }
+  }
+  
+  expect_true(is.list(treeItems))
+  expect_equal(length(unlist(treeItems)), 6)
+  expect_equal(depth(treeItems), 4)
+  
 })
