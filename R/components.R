777d5217fc7348243c836f079dba3bddb6432c06
lgnbhl <felix.luginbuhl@protonmail.ch>
2026-04-18 12:21:00 +0200

    add `print.muiTreeView`

--- R/components.R
+++ R/components.R
@@ -1,27 +1,28 @@
-## Script generated automatically from 'inst/helpers/generate_docs_tests.R' ##
-
 component <- function(name, module = '@mui/x-tree-view') {
-  function(...) shiny.react::reactElement(
-    module = module,
-    name = name,
-    props = shiny.react::asProps(...),
-    deps = muiTreeViewDependency()
-  )
+  function(...) {
+    tag <- shiny.react::reactElement(
+      module = module,
+      name = name,
+      props = shiny.react::asProps(...),
+      deps = muiTreeViewDependency()
+    )
+    class(tag) <- c("muiTreeView", class(tag))
+    tag
+  }
 }
 
- 
 #' @rdname SimpleTreeView
 #' @export
 SimpleTreeView <- component('SimpleTreeView')
- 
+
 #' @rdname RichTreeView
 #' @export
 RichTreeView <- component('RichTreeView')
- 
+
 #' @rdname TreeItem
 #' @export
 TreeItem <- component('TreeItem')
- 
+
 #' @rdname TreeItemIcon
 #' @export
 TreeItemIcon <- component('TreeItemIcon')
